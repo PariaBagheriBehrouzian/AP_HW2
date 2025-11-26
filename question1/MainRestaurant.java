aaec4a760a7aac0e29d239c57c727e375265dbf6
Paria Behrouzian <PariaBehrouzian@example.com>
2025-11-26 16:36:46 +0330

    initial commit - question1 and question2 placeholders

---
+++ question1/MainRestaurant.java
@@ -0,0 +1,1 @@
+public class MainRestaurant { public static void main(String[] args){ System.out.println("Question1 placeholder"); } }
---
+++ question2/BankApp.java
@@ -0,0 +1,1 @@
+public class BankApp { public static void main(String[] args){ System.out.println("Question2 placeholder"); } }
